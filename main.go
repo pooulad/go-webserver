3188e0ef47a7c1e1e03bee2b3ec227dab36e9ba5
pooulad <poooooladi@gmail.com>
2023-05-27 11:19:40 +0330

    complete source

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
 package main
 
-
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
-
-func helloHanler(){
-	
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w,"parseform() error :%v",err)
+		return
+	}
+	fmt.Fprintf(w,"post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w,"name is %s\n",name)
+	fmt.Fprintf(w,"address is %s\n",address)
 }
 
-func main(){
+func helloHanler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 error", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not allowd", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w,"hello here")
+}
+
+func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/",fileServer)
-	http.HandleFunc("/form",formHandler)
-	http.Handle("/hello",helloHanler)
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHanler)
 
-
-	fmt.PrintF("starting server ar port 8080 \n")
-	err := http.ListenAndServe("8080",nil)
+	fmt.Printf("starting server ar port 8080 \n")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.
+		log.Fatal(err)
 	}
 }
